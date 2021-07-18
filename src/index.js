b796fb893fa9c4445a08d0caa4d9057990f35cd4
mrandolph83 <mrandolph83@gmail.com>
2021-07-18 12:02:00 -0400

    commit tree fb99a0ab6e26a07b418781ba41c91b3a846f3106

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('%c HI', 'color: firebrick')
+
+const container = document.querySelector("#dog-image-container")
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+fetch (imgUrl)
+.then(resp => resp.json())
+.then(images => {
+    const imgs = images.message
+    let imgsArray =  imgs.map((img) => {
+    let i = `<img src=${img}>`
+    return i 
+})
+
+imgsArray.forEach(element => {
+    container.innerHTML += element
+});
+   
+})  
